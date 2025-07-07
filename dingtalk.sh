7a28047be08fb7b66267934ac316dd0b32bcd185
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2025-07-07 14:27:56 +0800

    fix(manifest): fix version detection error

--- dingtalk.sh
+++ dingtalk.sh
@@ -6,7 +6,8 @@ elif [[ "$XMODIFIERS" =~ ibus ]]; then
     [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
     [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
 fi
+VERSION=$(cat /app/extra/dingtalk/version)
 export QT_QPA_PLATFORM=xcb
-export QT_PLUGIN_PATH=/app/extra/dingtalk:$QT_PLUGIN_PATH
-cd /app/extra/dingtalk || exit 1
+export QT_PLUGIN_PATH=/app/extra/dingtalk/$VERSION:$QT_PLUGIN_PATH
+cd "/app/extra/dingtalk/$VERSION" || exit 1
 ./com.alibabainc.dingtalk "$@"
