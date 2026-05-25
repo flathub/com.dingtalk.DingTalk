8433392d49fb34b9d39c79902aeb884689274712
ffreeez <133223917+ffreeez@users.noreply.github.com>
2026-05-26 02:20:36 +0800

    Update dingtalk 8.1.0 (#32)

--- dingtalk.sh
+++ dingtalk.sh
@@ -6,8 +6,9 @@ elif [[ "$XMODIFIERS" =~ ibus ]]; then
     [ -z "$QT_IM_MODULE" ] && export QT_IM_MODULE=ibus
     [ -z "$GTK_IM_MODULE" ] && export GTK_IM_MODULE=ibus
 fi
-VERSION=$(cat /app/extra/dingtalk/version)
+
 export QT_QPA_PLATFORM=xcb
-export QT_PLUGIN_PATH=/app/extra/dingtalk/$VERSION:$QT_PLUGIN_PATH
-cd "/app/extra/dingtalk/$VERSION" || exit 1
+export QT_PLUGIN_PATH=/app/extra/dingtalk/release:$QT_PLUGIN_PATH
+export LD_PRELOAD=/app/lib/libdingtalk_ssl_peer_certificate_shim.so${LD_PRELOAD:+:$LD_PRELOAD}
+cd "/app/extra/dingtalk/release" || exit 1
 ./com.alibabainc.dingtalk "$@"
